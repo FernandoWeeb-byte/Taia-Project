ab9d9cad1592146c95a36dd6fdd168eef369f5b8
giuliamaia <gmaiaserafim@gmail.com>
2022-10-03 00:19:10 -0300

    sarsa n step

--- nstep_sarsa.py
+++ nstep_sarsa.py
@@ -107,6 +107,87 @@ def run_nstep_sarsa(env, episodes, nstep=1, lr=0.1, gamma=0.95, epsilon=0.1, ren
 
     return sum_rewards_per_ep, Q
 
+# Algoritmo "n-step SARSA", online learning
+# Atenção: os espaços de estados e de ações precisam ser discretos, dados por valores inteiros
+def run_nstep_sarsa_step(env, steps, nstep=1, lr=0.1, gamma=0.95, epsilon=0.1, render=False):
+    assert isinstance(env.observation_space, gym.spaces.Discrete)
+    assert isinstance(env.action_space, gym.spaces.Discrete)
+
+    num_actions = env.action_space.n
+    
+    # inicializa a tabela Q com valores aleatórios de -1.0 a 0.0
+    # usar o estado como índice das linhas e a ação como índice das colunas
+    Q = np.random.uniform(low = -1.0, high = 0.0, 
+                          size = (env.observation_space.n, num_actions))
+
+    gamma_array = np.array([ gamma**i for i in range(0,nstep)])
+    gamma_power_nstep = gamma**nstep
+
+    # para cada episódio, guarda sua soma de recompensas (retorno não-discontado)
+    sum_rewards_per_ep = []
+    
+    # loop principal
+    done = False
+    sum_rewards, reward = 0, 0
+    
+    next_state = env.reset()
+    # escolhe a próxima ação
+    next_action = choose_action(Q, next_state, num_actions, epsilon)
+
+    # históricos de: estados, ações e recompensas
+    hs = deque(maxlen=nstep)
+    ha = deque(maxlen=nstep)
+    hr = deque(maxlen=nstep)
+
+    # executa 1 episódio completo, fazendo atualizações na Q-table
+    for step in range(steps):
+        # exibe/renderiza os passos no ambiente, durante 1 episódio a cada mil e também nos últimos 5 episódios 
+        # if render and (i >= (episodes - 5) or (i+1) % 1000 == 0):
+        #     env.render()
+                    
+        # preparação para avançar mais um passo
+        # lembrar que a ação a ser realizada já está escolhida
+        state = next_state
+        action = next_action
+
+        # realiza a ação
+        next_state, reward, done, _ = env.step(action)
+        sum_rewards += reward
+
+        hs.append(state)
+        ha.append(action)
+        hr.append(reward)
+        
+        # se o histórico estiver completo, 
+        # vai fazer uma atualização no valor Q do estado mais antigo
+        if len(hs) == nstep:
+            if done: 
+                # para estados terminais
+                V_next_state = 0
+                sum_rewards = 0
+                # ao fim do episódio, atualiza o Q dos estados que restaram no histórico
+                laststeps = len(hs) # pode ser inferior ao "nstep", em episódios muito curtos
+                for j in range(laststeps-1,0,-1):
+                    hs.popleft()
+                    ha.popleft()
+                    hr.popleft()
+                    delta = ( sum(gamma_array[0:j]*hr) + 0 ) - Q[hs[0],ha[0]]
+                    Q[hs[0],ha[0]] += lr * delta
+            else:
+                # escolhe (antecipadamente) a ação do próximo estado
+                next_action = choose_action(Q, next_state, num_actions, epsilon)
+                # para estados não-terminais -- valor máximo (melhor ação)
+                V_next_state = Q[next_state,next_action]
+                sum_rewards_per_ep.append(sum_rewards)
+            # delta = (estimativa usando a nova recompensa) - estimativa antiga
+            delta = ( sum(gamma_array*hr) + gamma_power_nstep * V_next_state ) - Q[hs[0],ha[0]]
+            
+            # atualiza a Q-table para o par (estado,ação) de n passos atrás
+            Q[hs[0],ha[0]] += lr * delta
+            # fim do laço por episódio
+        
+    return sum_rewards_per_ep, Q
+
 
 
 if __name__ == "__main__":
